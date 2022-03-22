6490916aa81e12761aa42221e6e149ee16b855e4
Shuranchik <94410956z@gmail.com>
2022-03-23 03:55:32 +0800

    Sdelal2works

--- АиСД 1 курс/лаба 2 вариант 7/init.c
+++ АиСД 1 курс/лаба 2 вариант 7/init.c
@@ -1,14 +1,6 @@
 #include <stdio.h> 
 #include <string.h>
 #define N_ARRAY 255
-void MT_ADD_STATE() 
-{
-
-}
-void MT_ADD_RULE() 
-{ 
-
-}
 
 char storage[N_ARRAY]; int idx_lenta; int state; 
 char cache_input[80];
@@ -61,12 +53,199 @@ char cache_input[80];
         }
         printf("\n");
     }
-
-    void MT_Handler() 
+    void MT_ReWrite(char val)
     {
+        storage[idx_lenta] = val;
+    }
+    void MT_HandlerW1() 
+    {   
+        state = 1; 
 
+        while ( state != 0 ) 
+        {
+            switch (state)
+            {
+                case 1:
+                    switch ( MT_CurData() )
+                    {
+                        case '#':
+                            MT_ReWrite('1'); state = 1;
+                            MT_Right(); 
+                            break;
+                        case '0':
+                            MT_ReWrite('0'); state = 2; 
+                            MT_Left();
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 2;
+                            MT_Left();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch ( MT_CurData() )
+                    {
+                        case '#':
+                            MT_ReWrite('#'); state = 3;
+                            MT_Right(); 
+                            break;
+                        case '0':
+                            MT_ReWrite('0'); state = 2; 
+                            MT_Left();
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 2; 
+                            MT_Left();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch ( MT_CurData() )
+                    {
+                        case '#':
+                            MT_ReWrite('1'); state = 0;
+                            break;
+                        case '0':
+                            MT_ReWrite('0'); state = 4; 
+                            MT_Left();
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 3; 
+                            MT_Left();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch ( MT_CurData() )
+                    {
+                        case '#':
+                            MT_ReWrite('0');  state = 0;
+                            break;
+                        case '0':
+                            MT_ReWrite('0'); state = 4;
+                            MT_Right();
+                            break;
+                        case '1':
+                            MT_ReWrite('0'); state = 4;
+                            MT_Right();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
+    void MT_HandlerW3() 
+    {
+        state = 1; 
+
+        while ( state != 0 ) 
+        {
+            switch (state)
+            {
+                case 1:
+                    switch ( MT_CurData() )
+                    {
+                        case '0':
+                            MT_ReWrite('0'); state = 1;
+                            MT_Left(); 
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 1; 
+                            MT_Left();
+                            break;
+                        case '#':
+                            MT_ReWrite('#'); state = 2;
+                            MT_Right();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch ( MT_CurData() )
+                    {
+                        case '0':
+                            MT_ReWrite('0'); state = 3;
+                            MT_Right(); 
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 3; 
+                            MT_Right();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch ( MT_CurData() )
+                    {
+                        case '0':
+                            MT_ReWrite('0'); state = 4;
+                            MT_Right();
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 4; 
+                            MT_Right();
+                            break;
+                        case '#':
+                            MT_ReWrite('#'); state = 0; 
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch ( MT_CurData() )
+                    {
+                        case '0':
+                            MT_ReWrite('0');  state = 5;
+                            MT_Right();
+                            break;
+                        case '1':
+                            MT_ReWrite('1'); state = 5;
+                            MT_Right();
+                            break;
+                        case '#':
+                            MT_ReWrite('#'); state = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 5:
+                    switch ( MT_CurData() )
+                    {
+                        case '0':
+                            MT_ReWrite('#');  state = 5;
+                            MT_Right();
+                            break;
+                        case '1':
+                            MT_ReWrite('#'); state = 5;
+                            MT_Right();
+                            break;
+                        case '#':
+                            MT_ReWrite('#'); state = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     
 
 // 
@@ -74,7 +253,21 @@ int main()
 {
     MT_Init();
     MT_WriteWord(); 
+    MT_HandlerW3();
 
+    printf("\nOutput: \n");
+    for (int i = 0; i<N_ARRAY; i++) 
+    {
+        if ( idx_lenta == i )
+        {
+            printf("[%c]",storage[i], idx_lenta);
+        }
+        else 
+        { 
+            printf("%c",storage[i]);
+        }
+        
+    }
 
     return 0;
 }
